afd5fb662d3a314a9930a956cd774cd3a8f01a9a
comelyes <stas@baryshnikov.online>
2020-10-18 19:49:54 +0800

    first commit

---
+++ Assets/Example/Features/Add/ad/adFeature.cs
@@ -0,0 +1,30 @@
+﻿using ME.ECS;
+
+namespace Example.Features.Add {
+
+    using Components; using Modules; using Systems; using Features; using Markers;
+    using ad.Components; using ad.Modules; using ad.Systems; using ad.Markers;
+    
+    namespace ad.Components {}
+    namespace ad.Modules {}
+    namespace ad.Systems {}
+    namespace ad.Markers {}
+    
+    #if ECS_COMPILE_IL2CPP_OPTIONS
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.NullChecks, false),
+     Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false),
+     Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.DivideByZeroChecks, false)]
+    #endif
+    public sealed class adFeature : Feature {
+
+        protected override void OnConstruct() {
+            
+        }
+
+        protected override void OnDeconstruct() {
+            
+        }
+
+    }
+
+}
---
+++ Assets/Example/Features/Asd/AsdFeature.cs
@@ -0,0 +1,30 @@
+﻿using ME.ECS;
+
+namespace Example.Features {
+
+    using Components; using Modules; using Systems; using Features; using Markers;
+    using Asd.Components; using Asd.Modules; using Asd.Systems; using Asd.Markers;
+    
+    namespace Asd.Components {}
+    namespace Asd.Modules {}
+    namespace Asd.Systems {}
+    namespace Asd.Markers {}
+    
+    #if ECS_COMPILE_IL2CPP_OPTIONS
+    [Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.NullChecks, false),
+     Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false),
+     Unity.IL2CPP.CompilerServices.Il2CppSetOptionAttribute(Unity.IL2CPP.CompilerServices.Option.DivideByZeroChecks, false)]
+    #endif
+    public sealed class AsdFeature : Feature {
+
+        protected override void OnConstruct() {
+            
+        }
+
+        protected override void OnDeconstruct() {
+            
+        }
+
+    }
+
+}
